0bbac699f0e2e95923f44676320ae34ab8008a84
Rafael Mimoso <nayrakalinesv@gmail.com>
2018-12-05 22:04:35 -0300

    Dólar e horário dinâmico

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
 new Vue({
-    el: '#app',
+el: '#app',
     data: {
         nomeEmpresa: "Empresa LTDA",
         email: '',
         password: '',
-        dolar: 3.98,
+        dolar: 3.86,
         logins: {
             "ricardo@empresaltda.com.br": "654321",
             "admin@empresaltda.com.br": "123456"
-        }
+        },
+        gain: 0,
+        greetings: ""
     },
 
     computed: {
@@ -16,7 +18,25 @@ new Vue({
             return this.logins[this.email]
         },
         passwordValid(){
-           return this.logins[this.email] == this.password
+        return this.logins[this.email] == this.password
         }
+    },
+
+    mounted(){
+        axios.get('https://economia.awesomeapi.com.br/usd').then(response => {
+            this.dolar = response.data[0].ask
+            var lastAsk = response.data[9].ask
+            this.gain = this.dolar - lastAsk
+        })
+
+        var hour = new Date().getHours()
+        if(hour >= 6 && hour <= 12)
+            this.greetings = "Bom dia!"
+        else if (hour <= 18)
+            this.greetings = "Boa tarde!"
+        else 
+            this.greetings = "Boa noite!"
+        
     }
+
 })
